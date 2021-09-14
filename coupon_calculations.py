3e89ad507c1b7489fd1aeb5fd0d1cda217a9ec37
avesky <avesky@gmail.com>
2021-09-13 21:50:27 -0500

    Module 3 Topic 3 Assignment

--- coupon_calculations.py
+++ coupon_calculations.py
@@ -7,7 +7,8 @@ The purpose of this program:
 
 You are online shopping at SuperAwesomeCouponDealsAndStuff.com. They offer two types of discounts,
 cash-off coupons and percent discount coupons. You must add tax after applying coupons.
-If you have cash-off coupons, those must be applied first, then apply the percent discount coupons on the pre-tax amount.
+If you have cash-off coupons, those must be applied first,
+then apply the percent discount coupons on the pre-tax amount.
 
 The following is the order of calculations:
 
@@ -26,30 +27,33 @@ $15.99 - $5.00 = $10.99.
 30% off $10.99 to get $7.69
 Add tax at 6% to get $8.15
 Add shipping cost before tax $5.95 to get $14.10
-Do not forget to use variables and constants where appropriate. Using variaables, the first line might look like the following:
+Do not forget to use variables and constants where appropriate. Using variables,
+the first line might look like the following:
 cash_off_subtotal = price - cash_off
 """
 
 import constants
 
-#enter Purchase amount
+# enter Purchase amount
 purchase_amount = float(input('How much is your purchase? '))
 
-#deduct cash-off
+# deduct cash-off
 cash_off = float(input('How much is your Cash Discount ($5 or $10? $'))
 
 cash_off_subtotal = purchase_amount - cash_off
 
-#deduct percent discount
+# deduct percent discount
 percent_off = float(input('What is your percentage discount? (10, 15, or 20)? '))
 decimal_conversion = 1 - (percent_off*.01)
 
 percent_off_subtotal = cash_off_subtotal*decimal_conversion
 
-#calculate total including tax
+# calculate total including tax
 tax_calc = percent_off_subtotal*(constants.TAX+1)
 
-#add shipping to post-discount, pre-tax total
+# add shipping to post-discount, pre-tax total,
+
+shipping_cost = 0  # it said shipping_cost could be undefined so i declared it = 0 before the conditional
 if percent_off_subtotal < 10:
     shipping_cost = 5.95
 elif 10 <= percent_off_subtotal < 30:
@@ -59,13 +63,11 @@ elif 30 <= percent_off_subtotal < 50:
 elif percent_off_subtotal >= 50:
     shipping_cost = 0
 
-#calculate final total with discounts, tax, and shipping applied.
+# calculate final total with discounts, tax, and shipping applied.
 
 final_total = tax_calc + shipping_cost
 
-#keep 2 digits after decimal.
+# keep 2 digits after decimal.
 final_total = format(final_total, '.2f')
 
 print(f'Your total price including all discounts, tax and shipping is ${final_total}.  Thanks for giving us money!')
-
-
